eb5cfdf850e1bfa94fd0369914eeb430bab1b7a5
kbc0 <berkay.cetin@ug.bilkent.edu.tr>
2023-02-20 20:46:05 +0300

    Tested, methods working fine
    Missing menu options

--- testMain.java
+++ testMain.java
@@ -1,8 +1,19 @@
 public class testMain {
     public static void main(String[] args) {
         Array test = new Array(5);
-        System.out.println(test.indexSum());
+
+        int[] testArray = test.getArray();
+        for (int element : testArray) {
+            System.out.print(" " + element);
+        }
+
+        System.out.println("\n" + test.indexSum());
         System.out.println(test.maxOfArray());
         System.out.println(test.minOfArray());
+
+        average testAv = new average(testArray);
+        testAv.setAverage();
+        testAv.setDistance();
+        System.out.println(testAv.toString());
     }
 }
