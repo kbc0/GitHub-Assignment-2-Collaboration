bba7ee66e15f686b7daacb350eb0ba94b7811c26
R2N <113932366+R-2-N@users.noreply.github.com>
2023-02-20 22:51:49 +0300

    Merged the average and Array classes into one.

--- Array.java
+++ Array.java
@@ -1,5 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Array {
     // Given an array size, create an int array that contains random integers in the
@@ -8,9 +10,13 @@ public class Array {
     Random rand = new Random();
     private int[] intArray;
 
+    private double averageOfNumbers;
+    private double[] numberDistances;
+
     // constructor method
     public Array(int arraySize) {
         intArray = new int[arraySize];
+        this.numberDistances = new double[intArray.length];
         for (int i = 0; i < arraySize; i++) {
             intArray[i] = rand.nextInt(101);
         }
@@ -29,6 +35,43 @@ public class Array {
 
         }
         return s;
+    }
+
+    // Finding and returning the average of the array.
+    public double getAverage() {
+
+        // Some variables that will be useful later:
+        double numberSum = 0;
+
+        // Let's find the count of numbers and their sums.
+        for (Integer number : intArray) {
+            numberSum += number;
+        }
+
+        // Lastly, calculate the average.
+        this.averageOfNumbers = numberSum / intArray.length;
+
+        return this.averageOfNumbers;
+    }
+
+
+    // Returning a number of array which indicates the distance from the
+    // average for each number in the assigned array.
+    public double[] getDistance() {
+        // We will be adding a counter to determine which element we are in the element
+        // at the moment.
+        int count = 0;
+        for (Integer number : intArray) {
+            this.numberDistances[count] = number - this.getAverage();
+            count++;
+        }
+        
+        return this.numberDistances;
+    }
+    
+    // Printing the distances.
+    public String distanceToString() {
+        return Arrays.toString(this.getDistance());
     }
 
     // Method for seperately summing numbers at even and odd indices
@@ -69,7 +112,7 @@ public class Array {
         }
         return max;
     }
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
         //getting input from the user to determine array size
@@ -80,10 +123,7 @@ public class Array {
         Array array = new Array(arraySize);
         System.out.println("Your randomized array is  {" + array + " }");
         //creating average 
-        average average = new average(array.getArray());
         int userChoice = 0;
-        average.setAverage();
-        average.setDistance();
         
         while( userChoice !=5 )
         {
@@ -93,7 +133,7 @@ public class Array {
             System.out.println("3 - Display how each element of the array differs from the average.");
             System.out.println("4 - Find the sum of elements with odd- and even-numbered indexes.");
             System.out.println("5 - Exit");
-            System.out.print("Please choose operation :");
+            System.out.print("Please choose operation : ");
             userChoice = in.nextInt();
             in.nextLine();
 
@@ -107,16 +147,13 @@ public class Array {
             }
             else if (userChoice == 3)
             {
-                System.out.println("Each element of the array differs from the average as" + average);
+                System.out.println("Average of the array is " + array.getAverage() + " and each element of the array differs from the average as " + array.distanceToString());
             }
             else if (userChoice == 4)
             {
                 System.out.println(array.indexSum());
             }
         }
-        
-
-
         
     }
 }
